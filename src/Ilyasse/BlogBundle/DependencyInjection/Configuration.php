b3476f423560961c5773ae661b0b65841f6f5f64
king-il <fadil.ilyasse@gmail.com>
2016-12-30 05:13:29 +0000

    Update Configuration.php

--- src/Ilyasse/BlogBundle/DependencyInjection/Configuration.php
+++ src/Ilyasse/BlogBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ilyasse_blog');
+        $treeBuilder->root('ilyasse_blog');
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
